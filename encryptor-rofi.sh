9043ef837b858efa846cbf1650463803f639b07c
troy600 <bazartroy531@gmail.com>
2024-11-30 19:19:20 +0800

    encryptor rofi 2nd

--- encryptor-rofi.sh
+++ encryptor-rofi.sh
@@ -5,6 +5,10 @@ Filepath="file"
 
 ######
 
+operation() {
+    echo "encrypt|decrypt|directory_encrypt|directory_decrypt" | rofi -dmenu -p "select an operation" -sep "|"
+}
+
 ErrorNoFile() {
     echo "File: $1 not found"
 }
@@ -13,8 +17,30 @@ RofiFile() {
     ls | rofi -dmenu -p "Choose a file to encrypt"
 }
 
-main() {
-    cat $1
+encrypt() {
+    openssl enc -e -k $key -in "$1" -out "$2" -aes-256-ecb 2> temp.txt
+    rm temp.exe
 }
 
-main $(RofiFile)
+main() {
+    method=$(operation)
+    file=$(RofiFile)
+
+    if /bin/"["  -f "$file" ]; then
+        echo "echo something"
+    else
+        ErrorNoFile $file
+        exit
+    fi
+    case "$method" in
+        encrypt)
+            echo encrypt
+            ;;
+        decrypt)
+            echo decrypt
+            ;;
+    esac
+
+
+}
+main
